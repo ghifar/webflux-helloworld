45cf85eccea07e9c4351fa6838c7786d761211f3
Abudzar Al Ghiffari <abudzar.alghiffari@zeepos.com>
2020-05-05 16:54:12 +0700

    helloworld

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@RestController
+class testing{
+
+	@GetMapping("/")
+	public ResponseEntity<?> controller1(){
+
+		return ResponseEntity.ok("<h1>Helloworld!!</h1>");
+	}
+}
